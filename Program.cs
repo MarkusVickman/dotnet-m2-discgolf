87bb121a5deb46e2f7b02e73240f7cb4e7121f19
Markus <markus.vickman@hotmail.com>
2025-01-28 12:41:38 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Aktiverar och lägger till mvc
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//Lägger till stöd för statiska filer och routing
+app.UseStaticFiles();
+app.UseRouting();
+
+//Mappar routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
