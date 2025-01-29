7d38ed270d64384c369755d9a8989d7afe81ae34
Markus <markus.vickman@hotmail.com>
2025-01-29 12:22:41 +0100

    Lagt till formulär på indexsidan och på en egensida.
    formuläret på indexsidan dirigerar datan till addCourse-sidan för att formuläret ska skapas upp därefter.
    
    Lagt till formulär och menystyling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,9 +14,46 @@ namespace discgolf.Controllers
             return View(JsonObj);
         }
 
+        /* public IActionResult AddCourse()
+         {
+             int numberOfBaskets = TempData["NumberOfBaskets"] != null ? Convert.ToInt32(TempData["NumberOfBaskets"]) : 0;
+
+             // Skicka numberOfBaskets till vyn
+             ViewBag.NumberOfBaskets = numberOfBaskets;
+
+             return View();
+         }*/
+
+        public IActionResult Play()
+        {
+
+            return View();
+        }
+
         [Route("/om-sidan")]
         public IActionResult About()
         {
+            return View();
+        }
+
+        [Route("/AddCourse")]
+        [HttpPost]
+        public IActionResult AddCourse(int numberOf)
+        {
+            ViewData["NumberOfBaskets"] = numberOf;
+            // Hanterar klick
+
+            int numberOfBaskets = ViewData["NumberOfBaskets"] != null ? Convert.ToInt32(ViewData["NumberOfBaskets"]) : 0;
+
+            if (numberOfBaskets <= 0)
+            {
+                TempData["ErrorMessage"] = "Antalet korgar måste vara större än 0.";
+                return RedirectToAction("Index");
+
+            }
+            // Skicka numberOfBaskets till vyn
+            ViewBag.NumberOfBaskets = numberOfBaskets;
+
             return View();
         }
 
--- Models/DgCourses.cs
+++ Models/DgCourses.cs
@@ -1,14 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace discgolf.Models
 {
     public class DgCourses {
         //Egenskaper
 
+        [Display(Name = "ID")]
         public required int Id { get; set; }
+        
+        [Display(Name = "Banans namn")]
         public required String CourseName { get; set; }
 
+
+        [Display(Name = "Par")]
         public required int[] Basket { get; set; }
 
+
+        [Display(Name = "Kordinater")]
         public string? Location { get; set; }
 
     }
