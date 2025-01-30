5782b900fed4da5afe8035b300b815539d0ffe06
Markus <markus.vickman@hotmail.com>
2025-01-30 13:28:28 +0100

    försöker göra listor av objekt i controllern för att lättare kunna lägga till och ändra i data.
    har svårt med listor och hantering av json data korrekt.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,34 +48,84 @@ namespace discgolf.Controllers
             //Validera
             if (ModelState.IsValid)
             {
-                string jsonStr = JsonConvert.SerializeObject(model);
-                //TempData["new-course"] = jsonStr;
-                //TempData["new-course"] = model;
 
-                // Hämta värde från cookie
-                //if (Request.Cookies["createdCourses"] == null){
-                //}
+                /* var createdCourses = "[" + Request.Cookies["createdCourses"] + "]";
+                 int id = 0;
+
+                 if (createdCourses != null)
+                 {
+                     var JsonObj = JsonConvert.DeserializeObject<IEnumerable<DgCourses>>(createdCourses);
+                     id = JsonObj!.Count;
+                 }
+
+                 model.Id = id;
+
+                 string jsonStr = JsonConvert.SerializeObject(model);
+                 //TempData["new-course"] = jsonStr;
+                 //TempData["new-course"] = model;
+
+                 // Hämta värde från cookie
+                 //if (Request.Cookies["createdCourses"] == null){
+                 //}
+
+
+                 string courses = jsonStr + "," + createdCourses;*/
+
                 var createdCourses = Request.Cookies["createdCourses"];
+                int id = 0;
 
-                string courses = jsonStr + "," + createdCourses;
-
-                // Sätta värde i cookie
-                Response.Cookies.Append("createdCourses", courses, new CookieOptions
+                if (!string.IsNullOrEmpty(createdCourses))
                 {
-                    Expires = DateTime.Now.AddYears(2),
-                    HttpOnly = true,
-                    Secure = true
-                });
+                    // Deserialisera JSON-strängen till en lista av DgCourses-objekt
+                    var coursesList = JsonConvert.DeserializeObject<List<DgCourses>>(createdCourses);
 
-                /*  Response.Cookies.Append("createdCourses", "", new CookieOptions
-                  {
-                      Expires = DateTime.Now.AddDays(-1),
-                      HttpOnly = true,
-                      Secure = true
-                  });*/
+                    // Sätt ID till antalet objekt i listan
+                    id = coursesList!.Count;
 
+                    // Lägg till den nya kursen till listan
+                    model.Id = id;
+                    coursesList.Add(model);
+
+                    // Serialisera listan tillbaka till en JSON-sträng
+                    string courses = JsonConvert.SerializeObject(coursesList);
+
+                    // Sätta värde i cookie
+                    Response.Cookies.Append("createdCourses", courses, new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddYears(2),
+                        HttpOnly = true,
+                        Secure = true
+                    });
+
+                    /*  Response.Cookies.Append("createdCourses", "", new CookieOptions
+                      {
+                          Expires = DateTime.Now.AddDays(-1),
+                          HttpOnly = true,
+                          Secure = true
+                      });*/
+
+                }
+                else
+                {
+
+                    // Om createdCourses är null eller tom, initiera en ny lista och lägg till kursen
+                    var coursesList = new List<DgCourses> { model };
+
+                    // Serialisera listan tillbaka till en JSON-sträng
+                    string updatedCourses = JsonConvert.SerializeObject(coursesList);
+
+                    // Sätta värde i cookie
+                    Response.Cookies.Append("createdCourses", updatedCourses, new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddYears(2),
+                        HttpOnly = true,
+                        Secure = true
+                    });
+
+
+
+                }
                 return RedirectToAction("Index");
-
             }
             else
             {
@@ -86,6 +136,7 @@ namespace discgolf.Controllers
                 return View();
 
             }
+
 
         }
 
